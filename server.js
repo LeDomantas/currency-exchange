241b6be4f99696fca917b00ad2c9024ab0922d0f
Domantas Lekavicius <domantas.lekavicius@gmail.com>
2020-09-06 18:58:14 +0300

    fix: add unhandledRejection handling

--- server.js
+++ server.js
@@ -15,7 +15,12 @@ app.use('/api/v1/quote', quote);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
+
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  server.close(() => process.exit(1));
+});
