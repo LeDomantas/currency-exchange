043a1f0c7fe05082e4e1ff372eaba5a2a3fb6f0c
Domantas Lekavicius <domantas.lekavicius@gmail.com>
2020-09-06 11:07:06 +0300

    chore: add morgan logger middleware

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const quote = require('./routes/quote');
+const morgan = require('morgan');
 
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 app.use('/api/v1/quote', quote);
 
