1763328c4db5cc57d2725785767d8adc87ed044b
Domantas Lekavicius <domantas.lekavicius@gmail.com>
2020-09-06 23:44:47 +0300

    refactor: rename server.js to index.js, move some logic from index.js to app.js

--- app.js
+++ app.js
@@ -13,14 +13,4 @@ app.use('/api/v1/quote', quote);
 
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
-
-const server = app.listen(
-  PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
-);
-
-process.on('unhandledRejection', (err, promise) => {
-  console.log(`Error: ${err.message}`);
-  server.close(() => process.exit(1));
-});
+module.exports = app;
