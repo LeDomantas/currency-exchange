5da975171974ec4fa52dee465894e80fbe10b224
Domantas Lekavicius <domantas.lekavicius@gmail.com>
2020-09-06 18:57:54 +0300

    feat: add basic quote implementation

--- controllers/quote.js
+++ controllers/quote.js
@@ -1,6 +1,31 @@
+const axios = require('axios');
+const asyncHandler = require('../middleware/async');
+
 // @desc      Get quote
 // @route     GET /api/v1/quote
 // @access    Public
-exports.getQuote = (req, res, next) => {
-  res.status(200).json({ success: true, msg: 'Show quote' });
-};
+exports.getQuote = asyncHandler(async (req, res, next) => {
+  if (
+    req.query.base_currency &&
+    req.query.quote_currency &&
+    req.query.base_amount
+  ) {
+    const response = await axios.get(
+      `https://api.exchangeratesapi.io/latest?base=${req.query.base_currency}&symbols=${req.query.quote_currency}`
+    );
+    res.status(200).json({
+      exchange_rate: response.data.rates[req.query.quote_currency].toFixed(3),
+      qoute_amount: Math.floor(
+        req.query.base_amount *
+          response.data.rates[req.query.quote_currency] *
+          100
+      ),
+    });
+  } else {
+    res.status(400).json({
+      success: false,
+      msg:
+        'Please include base_currency, quote_currency and base_amount query params.',
+    });
+  }
+});
