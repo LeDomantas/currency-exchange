d678566449d8bb43e2a5f4946b42209062829edb
Domantas Lekavicius <domantas.lekavicius@gmail.com>
2020-09-06 23:06:16 +0300

    feat: add custom error message keys for front-end localization to QuouteQueryParamsSchema

--- models/QuoteQueryParams.js
+++ models/QuoteQueryParams.js
@@ -1,13 +1,39 @@
 const Joi = require('joi');
+const LOCALIZATION_KEYS = require('../utils/localizationKeys');
 
 const QuoteQueryParamsSchema = Joi.object({
   base_currency: Joi.string()
     .pattern(/^(USD|EUR|GBP|ILS)$/)
-    .required(),
+    .required()
+    .error((err) => {
+      switch (err[0].code) {
+        case 'string.pattern.base':
+          return new Error(LOCALIZATION_KEYS.NOT_SUPPORTED_BASE_CURRENCY);
+        case 'any.required':
+          return new Error(LOCALIZATION_KEYS.BASE_CURRENCY_REQUIRED);
+      }
+    }),
   quote_currency: Joi.string()
     .pattern(/^(USD|EUR|GBP|ILS)$/)
-    .required(),
-  base_amount: Joi.number().required(),
+    .required()
+    .error((err) => {
+      switch (err[0].code) {
+        case 'string.pattern.base':
+          return new Error(LOCALIZATION_KEYS.NOT_SUPPORTED_QUOTE_CURRENCY);
+        case 'any.required':
+          return new Error(LOCALIZATION_KEYS.QUOTE_CURRENCY_REQUIRED);
+      }
+    }),
+  base_amount: Joi.number()
+    .required()
+    .error((err) => {
+      switch (err[0].code) {
+        case 'number.base':
+          return new Error(LOCALIZATION_KEYS.BASE_AMOUNT_NOT_NUMBER);
+        case 'any.required':
+          return new Error(LOCALIZATION_KEYS.BASE_AMOUNT_REQUIRED);
+      }
+    }),
 });
 
 module.exports = QuoteQueryParamsSchema;
