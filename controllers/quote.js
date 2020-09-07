4c59c60fbdabfc2bbd498802873f336e62046f0e
Domantas Lekavicius <domantas.lekavicius@gmail.com>
2020-09-07 09:22:21 +0300

    feat: use LRUCache in getQuote handler

--- controllers/quote.js
+++ controllers/quote.js
@@ -2,6 +2,9 @@ const axios = require('../utils/axiosExchangeratesapi');
 const asyncHandler = require('../middleware/async');
 const ErrorResponse = require('../utils/errorResponse');
 const Quote = require('../models/QuoteQueryParams');
+const LRUCache = require('../models/LRUCache');
+
+const cache = new LRUCache(2);
 
 // @desc      Get quote
 // @route     GET /quote
@@ -11,9 +14,14 @@ exports.getQuote = asyncHandler(async (req, res, next) => {
   if (validation.error) {
     return next(new ErrorResponse(validation.error.message, 400));
   }
-  const response = await axios.get(
-    `/latest?base=${req.query.base_currency}&symbols=${req.query.quote_currency}`
-  );
+  const key = req.query.base_currency + req.query.quote_currency;
+  let response = cache.get(key);
+  if (!response) {
+    response = await axios.get(
+      `/latest?base=${req.query.base_currency}&symbols=${req.query.quote_currency}`
+    );
+    cache.put(key, response);
+  }
   res.status(200).json({
     exchange_rate: +response.data.rates[req.query.quote_currency].toFixed(3),
     quote_amount: Math.floor(
